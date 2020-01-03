925dfa1e92e2aafc1592f52f473dfd35a2860c0a
TheOriginalMomo <37563079+TheOriginalMomo@users.noreply.github.com>
2020-01-02 22:01:16 -0500

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,16 @@
+public class MyFirstJavaProgram{ //Class Name and the file name should be the same 
+								//CASE SENSITIVE
+								//NO PUBLIC CLASS; THEN FILE NAME CAN BE DIFFERENT
+/* This is my first java program.
+*This will print 'Hello World as the output
+*/
+
+public static void main(String []args) //mandatory for every java program. MAIN METHOD
+{
+System.out.println("Hello World"); //prints Hello World
+
+}
+
+
+
+}
